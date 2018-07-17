aa57a7e743592d04f4d265c3b8d6ba132e161f13
BOURBONCROW-PC\BourbonCrow <mattin.br@gmail.com>
2018-07-17 03:03:25 +0200

    Edited for EU quick and dirty

---
+++ static/js/bdobosstimer.js
@@ -0,0 +1,175 @@
+// https://fullcalendar.io/docs
+// http://ui.toast.com/tui-calendar
+// -- https://nhnent.github.io/tui.calendar/latest/index.html
+
+// Globals --------------------------------------------------------------------
+var calendar = null;
+var events = [];
+var current_time = moment();
+var image_path = 'static/images/';
+var next_boss = [];
+var utc_offset = 0; // GMT-0700
+var location_base_url = 'http://www.somethinglovely.net';
+var database_base_url = 'https://bddatabase.net'; // 'https://bdocodex.com'
+
+// msToTime -------------------------------------------------------------------
+function msToTime(duration) {
+    var seconds = parseInt((duration / 1000) % 60);
+    var minutes = parseInt((duration / (1000 * 60)) % 60);
+    var hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    //return hours + ":" + minutes + ":" + seconds;
+    return hours + "h " + minutes + "m " + seconds + "s";
+}
+
+// checkTime ------------------------------------------------------------------
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+// addEvents ------------------------------------------------------------------
+function addEvents() {
+    // Loop through raw events
+    for (var i = 0; i < raw_events.length; i++) {
+        // Init
+        var color = boss_mapping[raw_events[i]['type']]['color'];
+        var display_short = boss_mapping[raw_events[i]['type']]['display']['short'];
+        // Previous week
+        start = moment().utcOffset(utc_offset).startOf('week').subtract(7, 'days').add(raw_events[i]['start']);
+        event = {
+            'title': display_short,
+            'start': start,
+            'color': color,
+        };
+        events.push(event);
+        // Current week
+        start = moment().utcOffset(utc_offset).startOf('week').add(raw_events[i]['start']);
+        event = {
+            'title': display_short,
+            'start': start,
+            'color': color,
+        };
+        events.push(event);
+        // Next week
+        var start = moment().utcOffset(utc_offset).startOf('week').add(7, 'days').add(raw_events[i]['start']);
+        event = {
+            'title': display_short,
+            'start': start,
+            'color': color,
+        };
+        events.push(event);
+    }
+}
+
+// setCalendar ----------------------------------------------------------------
+function setCalendar() {
+    calendar = $('#eu-full-calendar-instance').fullCalendar({
+        header: {
+            // left: 'prev,next today',
+            // center: 'title',
+            // right: 'agendaDay,agendaWeek,month',
+            left: '',
+            center: '',
+            right: ''
+        },
+        // height: 644,
+        contentHeight: 595,
+        // contentHeight: 1123,
+        // aspectRatio: 2,
+        events: events,
+        defaultView: 'agendaWeek',
+        nowIndicator: true,
+        dayClick: function (date) {
+            console.log('dayClick', date.format());
+        },
+        timezone: 'local', // 'America/Los Angeles','Europe/Amsterdam'
+        ignoreTimezone: false,
+        navLinks: false, // can click day/week names to navigate views
+        editable: false,
+        selectable: false,
+        slotDuration: '01:00:00',
+        // slotDuration: '00:30:00',
+        defaultTimedEventDuration: '01:00:00',
+        // defaultTimedEventDuration: '00:30:00',
+        eventLimit: true, // allow "more" link when too many events
+        // eventRender: function (event, el) {
+        //     // render the timezone offset below the event title
+        //     if (event.start.hasZone()) {
+        //         el.find('.fc-title').after(
+        //             $('<div class="tzo"/>').text(event.start.format('Z'))
+        //         );
+        //     }
+        // },
+        // columnHeaderHtml: function (mom) {
+        //     if (mom.weekday() === 5) {
+        //         return '<b>Friday!</b>';
+        //     } else {
+        //         return '<i>' + mom.format('LLL') + '</i>';
+        //     }
+        // }
+        columnHeaderText: function (mom) {
+            return mom.format('ddd');
+        }
+    });
+}
+
+// checkNextBoss --------------------------------------------------------------
+function checkNextBoss() {
+    for (var i = 0; i < raw_events.length; i++) {
+        var start = moment().utcOffset(utc_offset).startOf('week').add(raw_events[i]['start']);
+        // var color = boss_mapping[raw_events[i]['type']]['color'];
+        var display_full = boss_mapping[raw_events[i]['type']]['display']['full'];
+        var image = image_path + boss_mapping[raw_events[i]['type']]['image'];
+        if ((next_boss.length === 0 && start > current_time) || (next_boss.length > 0 && start.isSame(next_boss[0].time))) {
+            next_boss_entry = {
+                name: display_full,
+                time: start,
+                image: image,
+                type: raw_events[i]['type']
+            };
+            next_boss.push(next_boss_entry);
+        }
+    }
+
+    console.log(next_boss);
+    document.getElementById('eu-1-next-boss-content-time').innerHTML = next_boss[0].time.local().format('dddd, MMMM Do YYYY @ hh:mm:ss A');
+    document.getElementById("eu-1-next-boss-content-image").src = next_boss[0].image;
+    document.getElementById('eu-1-next-boss-content-name').innerHTML = next_boss[0].name;
+    document.getElementById('eu-1-next-boss-content-meta-location-link').setAttribute("href", location_base_url + boss_mapping[next_boss[0]['type']]['meta']['location']);
+    document.getElementById('eu-1-next-boss-content-meta-guide-link').setAttribute("href", boss_mapping[next_boss[0]['type']]['meta']['guide']);
+    document.getElementById('eu-1-next-boss-content-meta-database-link').setAttribute("href", database_base_url + boss_mapping[next_boss[0]['type']]['meta']['database']);
+}
+
+// startTime ------------------------------------------------------------------
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers < 10
+    m = checkTime(m);
+    s = checkTime(s);
+
+    var current_time = moment();
+    document.getElementById('current-time').innerHTML = current_time.format('hh:mm:ss A');
+
+    var next_boss_eta = msToTime(next_boss[0].time.diff(current_time));
+    document.getElementById('eu-1-next-boss-content-eta').innerHTML = next_boss_eta;
+
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
+
+// Entry point ----------------------------------------------------------------
+addEvents();
+setCalendar();
+checkNextBoss();
+startTime();
---
+++ static/js/data.js
@@ -0,0 +1,412 @@
+// Boss Mapping ---------------------------------------------------------------
+var boss_mapping = {
+    karanda: {
+        color: '#943126',
+        display: {
+            short: 'Karanda',
+            full: 'Karanda'
+        },
+        meta: {
+            'database': '/us/npc/23060/',
+            'location': '/bdo/#worldbosses/karanda',
+            'guide': 'http://urzasarchives.com/bdo/guides/harpy-queen-karanda/',
+        },
+        image: 'karanda.png'
+    },
+    kzarka: {
+        color: '#5B2C6F',
+        display: {
+            short: 'Kzarka',
+            full: 'Kzarka, The Lord of Corruption'
+        },
+        meta: {
+            'database': '/us/npc/23001/',
+            'location': '/bdo/#worldbosses/kzarka',
+            'guide': 'http://urzasarchives.com/bdo/guides/how-to-world-field-boss-black-desert-online/page_4/',
+        },
+        image: 'kzarka.png'
+    },
+    nouver: {
+        color: '#21618C',
+        display: {
+            short: 'Nouver',
+            full: 'Shadow of Nouver'
+        },
+        meta: {
+            'database': '/us/npc/23054/',
+            'location': '/bdo/#worldbosses/nouver',
+            'guide': 'https://www.invenglobal.com/articles/736/bdo-guide-world-boss-nouver',
+        },
+        image: 'nouver.png'
+    },
+    kutum: {
+        color: '#117864',
+        display: {
+            short: 'Kutum',
+            full: 'Ancient Kutum'
+        },
+        meta: {
+            'database': '/us/npc/23073/',
+            'location': '/bdo/#worldbosses/ancient-kutum',
+            'guide': 'http://urzasarchives.com/bdo/guides/ancient-kutum-bossfight-guide/',
+        },
+        image: 'kutum.png'
+    },
+    offin: {
+        color: '#9A7D0A',
+        display: {
+            short: 'Offin',
+            full: 'Watcher Offin Tett'
+        },
+        meta: {
+            'database': '/us/npc/23754/',
+            'location': '/bdo/#worldbosses/offin-tett',
+            'guide': 'https://www.invenglobal.com/blackdesertonline/articles/1709/in-depth-raid-guide-for-mirumok-watcher-offin-the-awakened-ancient-weapon-in-kamasylvia',
+        },
+        image: 'offin.png'
+    },
+    quint: {
+        color: '#935116',
+        display: {
+            short: 'Quint',
+            full: 'Quint, The First Troll'
+        },
+        meta: {
+            'database': '/us/npc/23102/',
+            'location': '/bdo/#worldbosses/quint',
+            'guide': 'https://www.invenglobal.com/articles/3472/guide-on-the-new-world-bosses-muraka-and-quint-things-to-get-locations-patterns-and-rewards',
+        },
+        image: 'quint.png'
+    },
+    muraka: {
+        color: '#5F6A6A',
+        display: {
+            short: 'Muraka',
+            full: 'Muraka, King of Ogres'
+        },
+        meta: {
+            'database': '/us/npc/23097/',
+            'location': '/bdo/#worldbosses/muraka',
+            'guide': 'https://www.invenglobal.com/articles/3472/guide-on-the-new-world-bosses-muraka-and-quint-things-to-get-locations-patterns-and-rewards',
+        },
+        image: 'muraka.png'
+    },
+    vell: {
+        color: '#1C2833',
+        display: {
+            short: 'Vell',
+            full: 'Vell, Heart of the Ocean'
+        },
+        meta: {
+            'database': '/us/npc/23080/',
+            'location': '/bdo/#worldbosses/vell',
+            'guide': 'https://www.invenglobal.com/blackdesertonline/articles/4063/a-simple-guide-on-vell-the-new-ocean-raid-boss',
+        },
+        image: 'vell.png'
+    },
+};
+
+// Events ---------------------------------------------------------------------
+var raw_events = [
+    // ========== Day 0 : Sunday ==========
+    // 00:00
+    {
+        type: 'kzarka',
+        start: {days: 0, hours: 0, minutes: 0, seconds: 0},
+    },
+    // 03:00
+    {
+        type: 'kutum',
+        start: {days: 0, hours: 3, minutes: 0, seconds: 0},
+    },
+    // 07:00
+    {
+        type: 'nouver',
+        start: {days: 0, hours: 7, minutes: 0, seconds: 0},
+    },
+    // 10:00
+    {
+        type: 'kzarka',
+        start: {days: 0, hours: 10, minutes: 0, seconds: 0},
+    },
+    // 14:00
+    {
+        type: 'vell',
+        start: {days: 0, hours: 14, minutes: 0, seconds: 0},
+    },
+    // 17:00
+    {
+        type: 'karanda',
+        start: {days: 0, hours: 17, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kzarka',
+        start: {days: 0, hours: 17, minutes: 0, seconds: 0},
+    },
+    // 22:00
+    {
+        type: 'nouver',
+        start: {days: 0, hours: 22, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kutum',
+        start: {days: 0, hours: 22, minutes: 0, seconds: 0},
+    },
+    // ========== Day 1 : Monday ==========
+    // 00:00
+    {
+        type: 'karanda',
+        start: {days: 1, hours: 0, minutes: 0, seconds: 0},
+    },
+    // 03:00
+    {
+        type: 'kzarka',
+        start: {days: 1, hours: 3, minutes: 0, seconds: 0},
+    },
+    // 07:00
+    {
+        type: 'kzarka',
+        start: {days: 1, hours: 7, minutes: 0, seconds: 0},
+    },
+    // 10:00
+    {
+        type: 'nouver',
+        start: {days: 1, hours: 10, minutes: 0, seconds: 0},
+    },
+    // 14:00
+    {
+        type: 'kutum',
+        start: {days: 1, hours: 14, minutes: 0, seconds: 0},
+    },
+    // 17:00
+    {
+        type: 'nouver',
+        start: {days: 1, hours: 17, minutes: 0, seconds: 0},
+    },
+    // 22:00
+    {
+        type: 'karanda',
+        start: {days: 1, hours: 22, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kzarka',
+        start: {days: 1, hours: 22, minutes: 0, seconds: 0},
+    },
+    // ========== Day 2 : Tuesday ==========
+    // 00:00
+    {
+        type: 'kutum',
+        start: {days: 2, hours: 0, minutes: 0, seconds: 0},
+    },
+    // 03:00
+    {
+        type: 'kzarka',
+        start: {days: 2, hours: 3, minutes: 0, seconds: 0},
+    },
+    // 07:00
+    {
+        type: 'kutum',
+        start: {days: 2, hours: 7, minutes: 0, seconds: 0},
+    },
+    // 10:00
+    {
+        type: 'offin',
+        start: {days: 2, hours: 10, minutes: 0, seconds: 0},
+    },
+    // 14:00
+    {
+        type: 'nouver',
+        start: {days: 2, hours: 14, minutes: 0, seconds: 0},
+    },
+    // 17:00
+    {
+        type: 'karanda',
+        start: {days: 2, hours: 17, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'nouver',
+        start: {days: 2, hours: 17, minutes: 0, seconds: 0},
+    },
+    // 22:00
+    {
+        type: 'kutum',
+        start: {days: 2, hours: 22, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kzarka',
+        start: {days: 2, hours: 22, minutes: 0, seconds: 0},
+    },
+    // ========== Day 3 : Wednesday ==========
+    // 00:00
+    {
+        type: 'karanda',
+        start: {days: 3, hours: 0, minutes: 0, seconds: 0},
+    },
+    // 03:00
+    // Maintenance
+    // 07:00
+    {
+        type: 'karanda',
+        start: {days: 3, hours: 7, minutes: 0, seconds: 0},
+    },
+    // 10:00
+    {
+        type: 'nouver',
+        start: {days: 3, hours: 10, minutes: 0, seconds: 0},
+    },
+    // 14:00
+    {
+        type: 'kzarka',
+        start: {days: 3, hours: 14, minutes: 0, seconds: 0},
+    },
+    // 17:00
+    {
+        type: 'kutum',
+        start: {days: 3, hours: 17, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'karanda',
+        start: {days: 3, hours: 17, minutes: 0, seconds: 0},
+    },
+    // 22:00
+    {
+        type: 'karanda',
+        start: {days: 3, hours: 22, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kzarka',
+        start: {days: 3, hours: 22, minutes: 0, seconds: 0},
+    },
+    // ========== Day 4 : Thursday ==========
+    // 00:00
+    {
+        type: 'kutum',
+        start: {days: 4, hours: 0, minutes: 0, seconds: 0},
+    },
+    // 03:00
+    {
+        type: 'kzarka',
+        start: {days: 4, hours: 3, minutes: 0, seconds: 0},
+    },
+    // 07:00
+    {
+        type: 'kutum',
+        start: {days: 4, hours: 7, minutes: 0, seconds: 0},
+    },
+    // 10:00
+    {
+        type: 'nouver',
+        start: {days: 4, hours: 10, minutes: 0, seconds: 0},
+    },
+    // 14:00
+    {
+        type: 'kutum',
+        start: {days: 4, hours: 14, minutes: 0, seconds: 0},
+    },
+    // 17:00
+    {
+        type: 'offin',
+        start: {days: 4, hours: 17, minutes: 0, seconds: 0},
+    },
+    // 22:00
+    {
+        type: 'karanda',
+        start: {days: 4, hours: 22, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kzarka',
+        start: {days: 4, hours: 22, minutes: 0, seconds: 0},
+    },
+    // ========== Day 5 : Friday ==========
+    // 00:00
+    {
+        type: 'nouver',
+        start: {days: 5, hours: 0, minutes: 0, seconds: 0},
+    },
+    // 03:00
+    {
+        type: 'karanda',
+        start: {days: 5, hours: 3, minutes: 0, seconds: 0},
+    },
+    // 07:00
+    {
+        type: 'kutum',
+        start: {days: 5, hours: 7, minutes: 0, seconds: 0},
+    },
+    // 10:00
+    {
+        type: 'karanda',
+        start: {days: 5, hours: 10, minutes: 0, seconds: 0},
+    },
+    // 14:00
+    {
+        type: 'nouver',
+        start: {days: 5, hours: 14, minutes: 0, seconds: 0},
+    },
+    // 17:00
+    {
+        type: 'kzarka',
+        start: {days: 5, hours: 17, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kutum',
+        start: {days: 5, hours: 17, minutes: 0, seconds: 0},
+    },
+    // 22:00
+    {
+        type: 'nouver',
+        start: {days: 5, hours: 22, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kzarka',
+        start: {days: 5, hours: 22, minutes: 0, seconds: 0},
+    },
+    // ========== Day 6 : Saturday ==========
+    // 00:00
+    {
+        type: 'offin',
+        start: {days: 6, hours: 0, minutes: 0, seconds: 0},
+    },
+    // 03:00
+    {
+        type: 'nouver',
+        start: {days: 6, hours: 3, minutes: 0, seconds: 0},
+    },
+    // 07:00
+    {
+        type: 'kutum',
+        start: {days: 6, hours: 7, minutes: 0, seconds: 0},
+    },
+    // 10:00
+    {
+        type: 'nouver',
+        start: {days: 6, hours: 10, minutes: 0, seconds: 0},
+    },
+    // 14:00
+    {
+        type: 'quint',
+        start: {days: 6, hours: 14, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'muraka',
+        start: {days: 6, hours: 14, minutes: 0, seconds: 0},
+    },
+    // 17:00
+    {
+        type: 'karanda',
+        start: {days: 6, hours: 17, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kzarka',
+        start: {days: 6, hours: 17, minutes: 0, seconds: 0},
+    },
+    // 22:00
+    {
+        type: 'nouver',
+        start: {days: 6, hours: 22, minutes: 0, seconds: 0},
+    },
+    {
+        type: 'kutum',
+        start: {days: 6, hours: 22, minutes: 0, seconds: 0},
+    },
+];
